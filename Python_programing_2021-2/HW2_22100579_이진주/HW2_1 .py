877840097ffcaab5bcac15216f1fc2f0068d4b76
leeejjju <dlwlswn02027@gmail.com>
2021-09-28 20:01:46 +0900

    9/28

---
+++ Python_programing_2021-2/HW2_22100579_이진주/HW2_1 .py
@@ -0,0 +1,25 @@
+#HW2-1
+#두 개의 정수를 입력 받아서, 두 정수를 포함하여 그 사이에 존재하는 정수들의 합을 계산하여 출력하는 프로그램을 작성하시오. 
+#예를 들어서 -2과 3이 입력되면 -2 + -1 + 0 + 1 + 2 + 3 = 3 이라고 출력한다.
+
+
+num1 = int(input("첫번째 정수를 입력하세요 : "))
+num2 = int(input("두번째 정수를 입력하세요 : "))
+sum = 0
+
+
+if num1 > num2 : #역순 
+    for i in range( num1, num2-1, -1) :
+        print(i, end = '')
+        if i != num2 :
+                print(" + ", end='')
+        sum = sum+i
+            
+else : #정순 
+    for i in range( num1, num2+1, 1) :
+        print(i, end = '')
+        if i != num2 :
+            print(" + ", end='')
+        sum = sum+i
+
+print(' = ', sum)
---
+++ Python_programing_2021-2/HW2_22100579_이진주/HW2_2 .py
@@ -0,0 +1,39 @@
+#HW2-2
+#아래 표의 기준과 같이 점수와 성적 유형을 입력 받고 학점을 계산하는 프로그램을 작성하시오. (if문 사용)
+
+# Score(float)
+#Letter >> PF
+#100 ~ 90 >> A, PD(Pass with Distinction)
+#Below 90 ~ 80 >> B, Pass
+# Below 80 ~ 70 >> C, Pass
+# Below 70 ~ 60 >> D, Pass
+# Below 60 >> F, Fail
+
+
+score = float(input(">>> Input your score  : "))
+kind = input(">>> Input Letter(L) or PF(PF) : ")
+
+if kind == 'PF' :
+    if score >= 90 :
+        grade = 'PD(Pass with Distinction)'
+    elif score >= 60 :
+        grade = 'Pass'
+    else :
+        grade = 'Fail'
+elif kind == 'L' or kind == 'Letter' :
+    if score >= 90 :
+        grade = 'A'
+    elif score >= 80 :
+        grade = 'B'
+    elif score >= 70 :
+        grade = 'C'
+    elif score >= 60 :
+        grade = 'D'
+    else :
+        grade = 'F'
+
+print("Your grade is ", grade)
+
+
+
+    
