94279935598de38812bd6754fffa314cad5955d6
leeejjju <dlwlswn02027@gmail.com>
2022-06-01 23:36:47 +0900

    6/1

--- 2022-1/data_structure/HW15_22100579_JinjuLee.cpp
+++ 2022-1/data_structure/HW15_22100579_JinjuLee.cpp
@@ -14,11 +14,18 @@ class s_record{
       double score;
 
       public:
+      s_record(){};
       s_record(string nID, string nName, double nScore){ //constructor
             s_id = nID;
             name = nName;
             score = nScore;
       }
+      string getID()
+      {return s_id;}
+      string getName()
+      {return name;}
+      double getScore()
+      {return score;}
 };
 
 
@@ -26,29 +33,32 @@ class s_record{
 void insertion_sort(s_record s[], int n);
 void bubble_sort(s_record s[], int n);
 void selection_sort(s_record s[], int n);
-void quick_sort(s_record s[], int n);
+void quick_sort(s_record s[], int left, int right);
 void merge_sort(s_record s[], int n);
 void heap_sort(s_record s[], int n);
 
+void swap(s_record* a, s_record* b); //swap two records
 void show_thelist(s_record s[], int n); //show the list
 
 
 int main(){
-      s_record s_list[12] = { {"21900013", "Kim ", 6.5}, {"21900136", "Lee ", 8.8 }, {"21900333", "Park", 9.2 }, 
-                  { "21800442", "Choi", 7.1}, {"21900375", "Ryu ", 5.4  }, {"21700248", "Cho ", 6.3 },
-                  { "21700302", "Jung", 8.3}, {"21800255", "Han ", 6.9  }, {"21800369", "Kang", 6.3 },
-                  { "21900401", "Yang", 9.1}, {"21800123", "Moon", 8.1  }, {"21700678", "Seo ", 7.9 } };
+      s_record s_list[12] = { 
+            {"21900013", "Kim ", 6.5}, {"21900136", "Lee ", 8.8 }, {"21900333", "Park", 9.2 }, 
+            { "21800442", "Choi", 7.1}, {"21900375", "Ryu ", 5.4  }, {"21700248", "Cho ", 6.3 },
+            { "21700302", "Jung", 8.3}, {"21800255", "Han ", 6.9  }, {"21800369", "Kang", 6.3 },
+            { "21900401", "Yang", 9.1}, {"21800123", "Moon", 8.1  }, {"21700678", "Seo ", 7.9 } 
+      };
       int    n = 12;
 
       //insertion_sort(s_list, n);
-      bubble_sort(s_list, n);
-      //selection_sort(s_list, n);
+      //bubble_sort(s_list, n);
+      selection_sort(s_list, n);
 
       //quick_sort(s_list, 0, n-1);
       //merge_sort(s_list, n);
       //heap_sort(s_list, n); 
 
-      cout << "< The result of the sorting >" << endl;
+      cout << "\n< The result of the sorting >" << endl;
       show_thelist(s_list, n);
 
       return 0;
@@ -58,19 +68,55 @@ int main(){
 
 
 //6 sorting methods
+//insertion sort
 void insertion_sort(s_record s[], int n){
+      s_record tmp;
+      int index = 1;
+
+      for(int i = 1; i < n ; i++){ //start index
+            index = i;
+            tmp = s[index];
+            while(index > 0 && (tmp.getID() < s[index-1].getID())){
+                  s[index] = s[index-1];
+                  index --;
+            }
+            s[index] = tmp;
+      }
 
 }
 
+//bubble sort
 void bubble_sort(s_record s[], int n){
 
+      for(int i = n; i >= 0 ; i--){ //range
+            for(int j = 1; j < i; j++){ //sorting circles range
+                  if(s[j-1].getID() > s[j].getID()){
+                        swap(s[j-1], s[j]);
+                  } 
+            }
+      }
+
 }
 
+//selection sort
 void selection_sort(s_record s[], int n){
 
+      for(int i = 0; i < n; i++){ //start index
+            //find min value
+            s_record* min = &s[i];
+            for(int j = i+1; j < n; j++){ 
+                  if(min->getID() > s[j].getID()) min = &s[j];
+            }
+
+            swap(s[i], *min);
+            //cout << i+1 <<"st min value is " << s[i].getID() << ", " <<  s[i].getName() << endl;
+      }
 }
 
-void quick_sort(s_record s[], int n){
+
+//quick sort
+void quick_sort(s_record s[], int left, int right){
+      
 
 }
 
@@ -83,7 +129,18 @@ void heap_sort(s_record s[], int n){
 }
 
 
+//swap two records
+void swap(s_record* a, s_record* b){
+      s_record tmp = *a;
+      *a = *b;
+      *b = tmp;
+}
+
 //show the list
 void show_thelist(s_record s[], int n){
-      
+      for(int i = 0; i < n; i++){
+            cout << s[i].getID() << " : " << s[i].getName() << " : " << s[i].getScore() << endl;
+      }
+      cout << endl;
 }
+
---
+++ 2022-1/java_programming/testTest/src/pearLee/testOne/TestHashMap.java
@@ -0,0 +1,60 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+class TestHashMap{
+
+    public static void main(String[] args){
+        Map<String, Integer> a = new HashMap<String, Integer>();
+
+        a.put("Kim", 0);
+        a.put("Lee", 1);
+        a.put("Park", 20);
+        a.put("Choi", 3);
+
+
+        /*
+        System.out.println(a);
+        for(String name : a.keySet()){ //저장된 key값 확인
+            System.out.println("[Key]:" + name + " [Value]:" + a.get(name));
+        }
+        */
+
+        //Map<String, Integer> result = sortMapByValue(a);
+        a = sortMapByKey(a);
+        for (Map.Entry<String, Integer> entry : a.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", "
+                    + "Value: " + entry.getValue());
+        }
+
+
+    }
+
+    public static LinkedHashMap<String, Integer> sortMapByKey(Map<String, Integer> a) {
+        List<Map.Entry<String, Integer>> entries = new LinkedList<>(a.entrySet());
+        Collections.sort(entries, (o1, o2) -> o1.getKey().compareTo(o2.getKey()));
+
+        LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : entries) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+    public static LinkedHashMap<String, Integer> sortMapByValue(Map<String, Integer> a) {
+        List<Map.Entry<String, Integer>> entries = new LinkedList<>(a.entrySet());
+        Collections.sort(entries, (o1, o2) -> o1.getValue().compareTo(o2.getValue()));
+
+        LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : entries) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+
+}
+
